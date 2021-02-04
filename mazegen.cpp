5e0245d456661040e12abdbe7d57d936a79cd7f1
Mike Pellegrino <47326270+mkpellegrino@users.noreply.github.com>
2021-02-04 15:46:17 -0500

    Add files via upload

--- mazegen.cpp
+++ mazegen.cpp
@@ -14,9 +14,8 @@ bool signature=false;
 bool latex=true;
 bool game=false;
 int uniqueid=0;
+int number_of_meanies=1;
 using namespace std;
-
-
 
 class LineValue
 {
@@ -132,11 +131,13 @@ ostream & operator << (ostream &out, const Node &n)
 		{
 		  if( !isDuplicate( A, B, C, D ) )
 		    out << "<line"
+			<< " construct=\"wall\""
 			<< " id=\"wall." << uniqueid++ << "\""
 			<< " x1=\"" << 10 + A*30  << "\""
 			<< " y1=\"" << 10 + B*30  << "\""
 			<< " x2=\"" << 10 + C*30  << "\""
 			<< " y2=\"" << 10 + D*30  << "\""
+		      
 			<< " stroke-width=\"2\" stroke=\"#000000\"/>";
 		}
 	    }
@@ -373,7 +374,12 @@ int main()
 
   WIDTH++; // account for the missing column
   HEIGHT++; // account for the missing row
-  
+
+  if( game )
+    {
+      cerr << "How many AI in the maze?" << endl;
+      scanf("%d", &number_of_meanies );
+    }
   srand (time(NULL));
 
   if( latex )
@@ -394,22 +400,44 @@ int main()
   else
     {
       cout << "<html>" << endl;
-      cout << "<head><title>A Maze Game - (C) 2021 - Michael K. Pellegrino</title>" << endl;
+      cout << "<head><title>A Maze Amusement- (C) 2021 - Michael K. Pellegrino</title>" << endl;
       if( game )
 	{
-	  cout << "<script language=\"javascript\">var things=[];document.addEventListener('keydown', Kdown);function Kdown(e){var elm=document.getElementById(\"btn\");var x=elm.getAttribute('cx');var y=elm.getAttribute('cy');var K=e.keyCode;if(K==38||K==73){elm.setAttribute('cy',(1)*(y)-5);if(collision()==1)elm.setAttribute('cy',(1)*(y)+5);}else if(K==37||K==74){elm.setAttribute('cx',(1)*(x)-5);if(collision()==1)elm.setAttribute('cx',(1)*(x)+5);}else if(K==40||K==75){elm.setAttribute('cy',(1)*(y)+5);if(collision()==1)elm.setAttribute('cy',(1)*(y)-5);}else if(K==39||K==76){elm.setAttribute('cx',(1)*(x)+5);if(collision()==1)elm.setAttribute('cx',(1)*(x)-5);}if(winner()==1){alert(\"YOU'VE MADE IT!\");}}function onload(){var k=0;var elm;var duplicate=0;for(i=0; i<1920; i++){x=\"wall.\" + i;elm=document.getElementById(x);if(elm !=null)things[k++]=elm;}start_time=new Date();}function winner(){var s1=document.getElementById(\"btn\");var x=1*s1.getAttribute('cx');var y=1*s1.getAttribute('cy');if(x > 895 && y > 890)return 1;return 0;}function collision(){var s1=document.getElementById(\"btn\");s1x=1*s1.getAttribute('cx');s1y=1*s1.getAttribute('cy');var s2;for(i=0; i<things.length; i++){s2=things[i];var s2x1=1*s2.getAttribute('x1');var s2x2=1*s2.getAttribute('x2');var s2y1=1*s2.getAttribute('y1');var s2y2=1*s2.getAttribute('y2');var TLx;var TLy;var BRx;var BRy;if(s2x1==s2x2){TLx=1*s2x1-4;BRx=1*s2x2+4}else if(s2x1<s2x2){TLx=s2x1;BRx=s2x2;}else {TLx=s2x2;BRx=s2x1;}if(s2y1==s2y2){TLy=1*s2y1-4;BRy=1*s2y2+4;}else if(s2y1<s2y2){TLy=s2y1;BRy=s2y2;}else{TLy=s2y2;BRy=s2y1;}if((s1x <=BRx)&&(s1x >=TLx)&&(s1y <=BRy)&&(s1y >=TLy))return 1;}return 0;}</script></head><body onload=\"onload();\"><center>" << endl;
+
+	  cout << "<!-- " << 30*WIDTH << "x" << 30*HEIGHT << "-->" << endl;
+	  cout << "<script language=\"javascript\">"
+	    
+	       << "var my_interval_timer,my_clock_timer,walls=[],entities=[],ais=[],colliding_entities=[],tick=0;function Kup(t){entities[0].setAttribute(\"dir\",0)}function Kdown(t){var i=t.keyCode;38!=i&&73!=i?37!=i&&74!=i?40!=i&&75!=i?39!=i&&76!=i?32!=i?192!=i?entities[0].setAttribute(\"dir\",\"0\"):entities[1].setAttribute(\"dir\",\"2\"):entities[1].setAttribute(\"dir\",\"1\"):entities[0].setAttribute(\"dir\",\"3\"):entities[0].setAttribute(\"dir\",\"2\"):entities[0].setAttribute(\"dir\",\"4\"):entities[0].setAttribute(\"dir\",\"1\")}function onload(){walls=document.querySelectorAll('[construct=\"wall\"]'),entities=document.querySelectorAll('[construct=\"entity\"]'),ais=document.querySelectorAll('[construct=\"ai\"]'),my_interval_timer=setInterval(update_positions,7),my_clock_timer=setInterval(update_clock,1e3)}function update_clock(){document.getElementById(\"htmlclock\").innerHTML=\"Clock: \"+tick++}function check_for_winner(){Number(entities[0].getAttribute(\"cx\"))>" << 30*WIDTH-40 << " &&Number(entities[0].getAttribute(\"cy\"))>" << 30*HEIGHT-40 << " &&(clearInterval(my_interval_timer),clearInterval(my_clock_timer),document.getElementById(\"status\").innerHTML=\"<font size=+3><b><i>You've Won in \"+tick+\" seconds!</i></b></font>\")}function move_ai(t){var i=Number(t.getAttribute(\"dir\")),e=Number(t.getAttribute(\"steps_left\")),l=Number(t.getAttribute(\"cx\")),r=Number(t.getAttribute(\"cy\"));return l>"<< 30*WIDTH+5<<"&&r>"<< 30*HEIGHT-20<<"?(t.setAttribute(\"cx\",\"15\"),void t.setAttribute(\"cy\",\"40\")):l<15&&r<40?(t.setAttribute(\"cx\",\""<< 30*WIDTH+5 <<"\"),void t.setAttribute(\"cy\",\"" << 30*HEIGHT-20<< "\")):e>0?(1==i?0==willCollideWithWall(l,r-1)&&0==willAICollideWithEntity(l,r-1)?t.setAttribute(\"cy\",r-1):1==willAICollideWithEntity(l,r-1)?(entities[0].setAttribute(\"cx\",\"20\"),entities[0].setAttribute(\"cy\",\"25\")):e=1:2==i?0==willCollideWithWall(l,r+1)&&0==willAICollideWithEntity(l,r-1)?t.setAttribute(\"cy\",r+1):1==willAICollideWithEntity(l,r-1)?(entities[0].setAttribute(\"cx\",\"20\"),entities[0].setAttribute(\"cy\",\"25\")):e=1:3==i?0==willCollideWithWall(l+1,r)&&0==willAICollideWithEntity(l,r-1)?t.setAttribute(\"cx\",l+1):1==willAICollideWithEntity(l,r-1)?(entities[0].setAttribute(\"cx\",\"20\"),entities[0].setAttribute(\"cy\",\"25\")):e=1:4==i?0==willCollideWithWall(l-1,r)&&0==willAICollideWithEntity(l,r-1)?t.setAttribute(\"cx\",l-1):1==willAICollideWithEntity(l,r-1)?(entities[0].setAttribute(\"cx\",\"20\"),entities[0].setAttribute(\"cy\",\"25\")):e=1:t.setAttribute(\"dir\",Math.floor(4*Math.random())+1),void t.setAttribute(\"steps_left\",--e)):(t.setAttribute(\"steps_left\",Math.floor(60*Math.random())+1),void t.setAttribute(\"dir\",Math.floor(4*Math.random())+1))}function move(t){var i=t.getAttribute(\"dir\");if(0!=i){var e=Number(t.getAttribute(\"cx\")),l=Number(t.getAttribute(\"cy\"));return 1==i?willCollideWithWall(e,l-1)?void t.setAttribute(\"dir\",\"0\"):willCollideWithEntity(e,l-1)?(colliding_entities[0].setAttribute(\"dir\",\"0\"),void colliding_entities[1].setAttribute(\"dir\",\"0\")):void t.setAttribute(\"cy\",l-1):2==i?willCollideWithWall(e,l+1)?void t.setAttribute(\"dir\",\"0\"):willCollideWithEntity(e,l+1)?(colliding_entities[0].setAttribute(\"dir\",\"0\"),void colliding_entities[1].setAttribute(\"dir\",\"0\")):void t.setAttribute(\"cy\",l+1):3==i?willCollideWithWall(e+1,l)?void t.setAttribute(\"dir\",\"0\"):willCollideWithEntity(e+1,l)?(colliding_entities[0].setAttribute(\"dir\",\"0\"),void colliding_entities[1].setAttribute(\"dir\",\"0\")):void t.setAttribute(\"cx\",e+1):4==i?willCollideWithWall(e-1,l)?void t.setAttribute(\"dir\",\"0\"):willCollideWithEntity(e-1,l)?(colliding_entities[0].setAttribute(\"dir\",\"0\"),void colliding_entities[1].setAttribute(\"dir\",\"0\")):void t.setAttribute(\"cx\",e-1):void 0}}function update_positions(){for(var t=0;t<entities.length;t++)move(entities[t]);check_for_winner();for(t=0;t<ais.length;t++)move_ai(ais[t])}function willAICollideWithEntity(t,i){return 20==Number(entities[0].getAttribute(\"cx\"))&&25==Number(entities[0].getAttribute(\"cy\"))?0:Math.abs(Number(entities[0].getAttribute(\"cx\"))-t)<9&&Math.abs(Number(entities[0].getAttribute(\"cy\"))-i)<9?1:0}function willCollideWithEntity(t,e){var l=0,r=0,n=0;for(i=0;i<entities.length;i++){var s=Number(entities[i].getAttribute(\"cx\")),u=Number(entities[i].getAttribute(\"cy\"));if(Math.abs(s-t)<10&&Math.abs(u-e)<10&&(l++,0==r&&(r=i),n=i),l>1)return colliding_entities[0]=entities[r],colliding_entities[1]=entities[n],1}return 0}function willCollideWithWall(t,e){for(i=0;i<walls.length;i++){var l,r,n,s,u=Number(walls[i].getAttribute(\"x1\")),o=Number(walls[i].getAttribute(\"x2\")),d=Number(walls[i].getAttribute(\"y1\")),c=Number(walls[i].getAttribute(\"y2\"));if(u==o?(l=u-4,n=o+4):u<o?(l=u,n=o):(l=o,n=u),d==c?(r=d-4,s=c+4):d<c?(r=d,s=c):(r=c,s=d),t<=n&&t>=l&&e<=s&&e>=r)return 1}return 0}document.addEventListener(\"keydown\",Kdown),document.addEventListener(\"keyup\",Kup);"
+	    
+	       << "</script></head><body onload=\"onload();\"><center>" << endl;
+
+
+	  
 	}
       else
 	{
 	  cout << "</head>" << endl;
 	}      
+      cout << "<center>"
+	   << "<p align=\"center\" id=\"title\"><font size=+4><b>Maze Amusement</font></p>"
+	   << "<p align=\"center\"><font size=+2>(C) 2021 - Michael K. Pellegrino</b></font></p>"
+	   << "<p align=\"center\">Use Arrow Keys or I (up), K (down), J (left), or L (right)</p>"
+	   << "<p align=\"center\" id=\"status\"></p>"
+	   << "<svg width=\"500\" height=\"500\" viewBox=\"0 0 " << (WIDTH)*30+10<< " " << (HEIGHT)*30+1<< " \">"
+	   << "<text x=\"0\" y=\"30\">Start</text>"
+	   << "<text x=\"" << (WIDTH-1.5)*30+10 << "\" y=\""  << (HEIGHT-1)*30 << "\">End</text>" 
+	   << "<circle dir=\"0\" id=\"player1\" construct=\"entity\" r=\"5\" cx=\"15\" cy=\"25\" fill=\"red\"/>"
+	   << "<line construct=\"wall\" id=\"wall.x\" x1=\"10\" y1=\"10\" x2=\"10\" y2=\"40\" stroke-width=\"2\" stroke=\"#000000\"/>";
 
-      cout << "<center>A Maze Amusement - Use Arrow Keys or I (up), K (down), J (left), or L (right)<br>" << endl;
-      cout << "<svg width=\"" << 500 << "px\" height=\"" << 500 << "px\" version=\"1.1\" viewBox=\"" << 0 << " " << 0 << " " << WIDTH*30+10 << " " << HEIGHT*30+10 << "\"><defs></defs>" << endl;
-      cout << "<circle id=\"btn\" r=\"5\" cx=\"25\" cy=\"25\" fill=\"red\"/>" << endl;
-      cout << "<text x=\"0\" y=\"30\">Start</text>" << endl;
-      cout << "<text x=\"" << (WIDTH-1.5)*30+10 << "\" y=\""  << (HEIGHT-1)*30 << "\">End</text>" << endl;
-    }
+      for( int i=0; i<number_of_meanies; i++ )
+	{
+	  int meanie_x = 30*(rand() % (WIDTH-1));
+	  int meanie_y = 30*(rand() % (HEIGHT-1));
+	  
+	  cout << "<circle dir=\"0\" id=\"ai" << (i+1) << "\" construct=\"ai\" r=\"5\" cx=\"" << meanie_x << "\" cy=\"" << meanie_y <<  "\" fill=\"blue\"/>";
+	}
+
+  }
   Field * f = new Field( WIDTH, HEIGHT );
 
 
@@ -560,10 +588,9 @@ int main()
     }
   else
     {
-      cout << "</svg>" << endl;
-      cout << "<p><font size=-2><i>(C) 2021 - Michael K. Pellegrino - <a target=_blank href=\"https://github.com/mkpellegrino/mazegen\">github.com/mkpellegrino/mazegen</a></i></font></p>" << endl;
-      cout << "<p><font size=-2><i>mazegen 1.0 - 2021 01 29</i></font></p>" << endl; 
-      cout << "</html>" << endl;
+      cout << "</svg>"
+	   << "<p align=\"center\" id=\"htmlclock\">Clock: 0</p>"
+	   << "</html>" << endl;
     }
   delete f;
   return 0;
